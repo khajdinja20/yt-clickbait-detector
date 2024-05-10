a5d55a7a0b3b66bb2d6b19ec190302a97547d359
Teodor Kolar <tkolar20@foi.hr>
2024-05-10 17:15:10 +0200

    Implemented basic image classification.

---
+++ src/content.js
@@ -0,0 +1,85 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>
+{
+    if(!message)
+    {
+        return;
+    }
+    switch(message.action)
+    {
+        case "IMAGE_CLICKED":
+            {
+                //findElement(message.url);
+                loadImageAndSendDataBack(findElement(message.url)[0], sendResponse);
+            }
+    }
+    return true;
+});
+
+
+function findElement(url)
+{
+    const lastIndex = url.lastIndexOf("/");
+    const watchURL = url.slice(lastIndex);
+    const wantedAnchors = document.querySelectorAll("a[href*='" + watchURL + "']");
+    let titleAnchor = wantedAnchors[1];
+    if(titleAnchor.id == "thumbnail")
+    {
+        titleAnchor = wantedAnchors[2];
+    }
+    const thumbnailURL = constructThumbnailURL(watchURL);
+    const title = titleAnchor.getAttribute("title");
+    console.log(thumbnailURL);
+    console.log(title);
+    return new Array(thumbnailURL, title);
+}
+
+function constructThumbnailURL(url)
+{
+    const firstSliceIndex = url.lastIndexOf("v=");
+    const tempURL = url.slice(firstSliceIndex + 2);
+    const annoyingPP = tempURL.lastIndexOf("&pp");
+    let videoID;
+    if(annoyingPP == -1)
+    {
+        videoID = tempURL;
+    }
+    else
+    {
+        videoID = tempURL.slice(0, annoyingPP);
+    }
+    return "https://img.youtube.com/vi/" + videoID + "/mqdefault.jpg";
+}
+
+function loadImageAndSendDataBack(src, sendResponse)
+{
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.onerror = function(e)
+    {
+        console.warn(`Could not load image from external source ${src}.`);
+        sendResponse({rawImageData: undefined});
+        return;
+    };
+    img.onload = function(e)
+    {
+        if((img.height && img.height > 120) ||
+            (img.width && img.width > 120))
+        {
+            img.width = 224;
+            img.height = 224;
+            const canvas = new OffscreenCanvas(img.width, img.height);
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+            const imageData = ctx.getImageData(0, 0, img.width, img.height);
+            sendResponse({
+                rawImageData: Array.from(imageData.data),
+                width: img.width,
+                height: img.height,
+            });
+            return;
+        }
+        console.warn(`Image size too small. [${img.height} x ${img.width}] vs. minimum [${MIN_IMG_SIZE} x ${MIN_IMG_SIZE}]`);
+        sendResponse({rawImageData: undefined});
+    };
+    img.src = src;
+}
--- src/service_worker.js
+++ src/service_worker.js
@@ -1,22 +1,102 @@
-chrome.contextMenus.onClicked.addListener(genericOnClick);
+//chrome.contextMenus.onClicked.addListener(genericOnClick);
+import * as tf from '@tensorflow/tfjs';
 
-// A generic onclick callback function.
-function genericOnClick(info) {
-    console.log('Standard context menu item clicked.');
+function preprocessImage(imageData)
+{
+    const pixels = tf.browser.fromPixels(imageData);
+    const img = tf.image.resizeBilinear(pixels, [224, 224]);
+    const normalizedImg = img.div(255.0);
+    const imgArray = normalizedImg.expandDims();
+    return imgArray;
 }
 
-chrome.runtime.onInstalled.addListener(function () {
-    // Create one test item for each context type.
+function clickMenuCallBack(info, tab)
+{
+    const message = {action: 'IMAGE_CLICKED', url: info.linkUrl};
+    console.log(info.linkUrl);
+    chrome.tabs.sendMessage(tab.id, message, (resp) =>
+    {
+        if(!resp.rawImageData)
+        {
+            console.error(
+                'Failed to get image data. ' +
+                'The image might be too small or failed to load. ' +
+                'See console logs for errors.');
+            return;
+        }
+        const imageData = new ImageData(Uint8ClampedArray.from(resp.rawImageData), resp.width, resp.height);
+        const classifierData = preprocessImage(imageData)
+        console.log(classifierData)
+        classifier.analyzeImage(classifierData, info.srcUrl, tab.id);
+    });
+
+}
+
+chrome.runtime.onInstalled.addListener(function()
+{
     let contexts = [
         'link'
     ];
-    for (let i = 0; i < contexts.length; i++) {
+    for(let i = 0; i < contexts.length; i++)
+    {
         let context = contexts[i];
-        let title = "Classify video";
+        let title = "Classify Video";
         chrome.contextMenus.create({
+            targetUrlPatterns: ["https://www.youtube.com/watch?v=*", "https://www.youtu.be/watch?v=*"],
             title: title,
             contexts: [context],
             id: context
         });
     }
 });
+
+chrome.contextMenus.onClicked.addListener(clickMenuCallBack);
+
+class Classifier
+{
+    constructor ()
+    {
+        this.loadModel();
+    }
+
+    async loadModel()
+    {
+        console.log("Loading model...");
+        try
+        {
+            //this.model = await tf.loadLayersModel("https://raw.githubusercontent.com/tkolar20/tkolar20.github.io/main/model/model.json");
+            //this.model = await tf.loadGraphModel("https://tkolar20.github.io/model_graph/model.json");
+            //this.model = await tf.loadGraphModel("https://tkolar20.github.io/model_graph_h5/model.json");
+            this.model = await tf.loadLayersModel("https://tkolar20.github.io/model_layers_h5/model.json");
+            tf.tidy(() => 
+            {
+                this.model.predict(tf.zeros([1, 224, 224, 3]));
+            });
+            console.log("Model loaded.");
+        }
+        catch(e)
+        {
+            console.log("Failed to load model", e);
+        }
+    }
+
+    async analyzeImage(imageData, url, tabId)
+    {
+        if(!tabId)
+        {
+            console.error('No tab.  No prediction.');
+            return;
+        }
+        console.log("Making prediction...");
+        const startTime = performance.now();
+        const predictions = await this.model.predict(imageData);
+        const totalTime = performance.now() - startTime;
+        console.log(`Done in ${totalTime.toFixed(1)} ms `);
+        const message = {action: 'IMAGE_CLICK_PROCESSED', url, predictions};
+        const predictedValue = predictions.arraySync()[0][0];
+        console.log(predictedValue);
+        //console.log(JSON.stringify(predictions));
+    }
+}
+
+const classifier = new Classifier();
