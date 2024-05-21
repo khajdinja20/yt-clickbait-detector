fc5843ecdf562169ecbbb12ccbdcfdf8305c035b
Teodor Kolar <tkolar20@foi.hr>
2024-05-21 14:23:02 +0200

    Start implement title predict., added instructions for tokens.

--- src/service_worker.js
+++ src/service_worker.js
@@ -23,7 +23,7 @@ function clickMenuCallBack(info, tab) {
         console.log(resp);
         const imageData = new ImageData(Uint8ClampedArray.from(resp.rawImageData), resp.width, resp.height);
         const classifierData = preprocessImage(imageData);
-        classifier.analyzeImage(classifierData, resp.title, info.linkUrl, tab.id);
+        classifier.analyze(classifierData, resp.title, info.linkUrl, tab.id);
     });
 
 }
@@ -54,17 +54,22 @@ class Classifier {
     async loadModel() {
         console.log("Loading model...");
         try {
-            if (await this.checkModelExists()) {
-                this.model = await tf.loadLayersModel("indexeddb://image-model");
+            if (false /*await this.checkModelExists() */) {
+                //this.model = await tf.loadLayersModel("indexeddb://image-model");
             }
             else {
-                this.model = await tf.loadLayersModel("https://tkolar20.github.io/model_layers_h5/model.json");
-                // for title model
-                // this.model = await tf.loadGraphModel("https://raw.githubusercontent.com/tkolar20/tkolar20.github.io/main/title_model_saved_graph/model.json");
-                await this.model.save("indexeddb://image-model");
+                //this.model = await tf.loadLayersModel("https://tkolar20.github.io/model_layers_h5/model.json");
+                this.titleModel = await tf.loadGraphModel("https://raw.githubusercontent.com/tkolar20/tkolar20.github.io/main/title_model_saved_graph/model.json");
+                //await this.model.save("indexeddb://image-model");
             }
             tf.tidy(() => {
-                this.model.predict(tf.zeros([1, 224, 224, 3]));
+                //this.model.predict(tf.zeros([1, 224, 224, 3]));
+                let input = "YOU WONT BELIEVE THIS!!!";
+                input = input.split(" ");
+                console.log(input);
+                const prediction = this.titleModel.execute(input);
+
+                console.log(prediction);
             });
             console.log("Model loaded.");
         }
@@ -73,16 +78,19 @@ class Classifier {
         }
     }
 
-    async analyzeImage(imageData, title, url, tabId) {
+    async analyze(imageData, title, url, tabId) {
         if (!tabId) {
             console.error('No tab.  No prediction.');
             return;
         }
         const startTime = performance.now();
         const predictions = await this.model.predict(imageData);
+        const titlePredictions = await this.titleModel.predict(title);
         const totalTime = performance.now() - startTime;
         console.log(`Done in ${totalTime.toFixed(1)} ms `);
         const predictedValue = predictions.arraySync()[0][0];
+        const titlePredictedValue = titlePredictions.arraySync()[0][0];
+        console.log(titlePredictedValue);
         const message = { action: 'IMAGE_CLICK_PROCESSED', title, url, predictedValue };
         chrome.tabs.sendMessage(tabId, message);
     }
@@ -115,6 +123,24 @@ class Classifier {
                 return resolve(false);
             };
         });
+    }
+}
+
+class Tokenizer {
+    constructor() {
+        this.loadTokenizer();
+    }
+
+    async loadTokenizer() {
+        // fetch vocab.txt and turn into array
+        // maybe fetch special_tokens_map and tokenizer_config
+    }
+
+    async tokenize() {
+        // turn title to array
+        // map each word to appropriate value(index from vocab.txt)
+        // on collab it sets first value to 101 and last to 102(dunno why)
+        // attention mask set all ones to word count + 2(i guess)
     }
 }
 
